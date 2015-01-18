bbcb2804630f2641582f87448007c4f25b2a7a25
Luciano Lattes <luciano.lattes@mulesoft.com>
2015-01-18 17:03:40 -0300

    Include borders in category generation (cut).

--- Exploratory/Week2/ggplot2Demo.R
+++ Exploratory/Week2/ggplot2Demo.R
@@ -7,7 +7,7 @@ england <- transform(england, Goals.against = as.numeric(Goals.against))
 england <- transform(england, Won = as.numeric(Won))
 # Generate quartiles for the number of matches won.
 wonQuartiles <- quantile(england$Won, seq(0, 1, length = 5), na.rm = TRUE)
-england$wonQuartiles <- cut(england$Won, wonQuartiles)
+england$wonQuartiles <- cut(england$Won, wonQuartiles, include.lowest = TRUE)
 # Make a plot using ggplot function faceted by matches won.
 g <- ggplot(england, aes(Pos, Goals.for))
 g + geom_point(alpha = 1/3) + geom_smooth(method="lm", se=TRUE, col="steelblue") + theme_bw(base_size = 12) + labs(x = "Position") + labs(y = "Goals for") + labs(title = "English PL Standings") + facet_grid(. ~ wonQuartiles)
