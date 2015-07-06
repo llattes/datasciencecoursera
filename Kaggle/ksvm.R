97fad55e048b49e4852a10803bc1320db244478d
Luciano Lattes <luciano.lattes@mulesoft.com>
2015-07-06 02:58:06 -0300

    KSVM!

--- Kaggle/ksvm.R
+++ Kaggle/ksvm.R
@@ -8,13 +8,13 @@ test <- read_csv("input/test.csv")
 
 train$label <- as.factor(train$label)
 
-numTrain <- 5000
+numTrain <- 6000
 set.seed(13)
 rows <- sample(1:nrow(train), numTrain)
 train2 <- train[rows, ]
 
-filter <- ksvm(label ~ ., data = train2, kernel = "polydot", kpar = list(degree = 2), cross = 3)
+filter <- ksvm(label ~ ., data = train2, kernel = "polydot", kpar = list(degree = 2), cross = 5, tol = 0.005)
 labels <- predict(filter, test)
 
 predictions <- data.frame(ImageId=1:nrow(test), Label=levels(train$label)[labels])
-write_csv(predictions, "output/polydot_d2_c3_5000.csv")
+write_csv(predictions, "output/polydot_d2_c5_6000_2.csv")
